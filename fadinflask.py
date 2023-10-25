1bbc02d1703f421eb1dc62e6b3a0c7eec79cb6d2
Mohammed Fadin <mfadin@sa.ibm.com>
2023-10-25 16:01:19 +0400

    UPDATE add v2

--- fadinflask.py
+++ fadinflask.py
@@ -4,9 +4,9 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 port = int(os.environ.get('FLASK_RUN_PORT', 80))
 
-@app.route('/hello', methods=['GET'])
+@app.route('/', methods=['GET'])
 def hello():
-    return "Hello Fadin v2!"
+    return "Hello Fadin v3!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
